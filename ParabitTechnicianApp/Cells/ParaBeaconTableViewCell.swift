34fe4f6940a81a571e715e9331a727ccff8a0fb0
Gurinder Singh <gsingh622@yahoo.com>
2017-10-05 17:14:21 -0400

    init

---
+++ ParabitTechnicianApp/Cells/ParaBeaconTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ParaBeaconTableViewCell.swift
+//  ParabitTechnicianApp
+//
+//  Created by Gurinder Singh on 10/5/17.
+//  Copyright © 2017 Gurinder Singh. All rights reserved.
+//
+
+import UIKit
+
+class ParaBeaconTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var statusBubbleImageView: UIImageView!
+    @IBOutlet weak var beaconNameLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ParabitTechnicianApp/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  ParabitTechnicianApp
+//
+//  Created by Gurinder Singh on 10/5/17.
+//  Copyright © 2017 Gurinder Singh. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}

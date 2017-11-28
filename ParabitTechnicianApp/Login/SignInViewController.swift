47345bf9378df35f7bedefe18fd763027e4aa521
Gurinder Singh <gsingh@Gurinders-MacBook-Pro.local>
2017-11-28 15:14:04 -0500

    -updated constraints for keyboard typing on sign in page

--- ParabitTechnicianApp/Login/SignInViewController.swift
+++ ParabitTechnicianApp/Login/SignInViewController.swift
@@ -42,6 +42,34 @@ class SignInViewController: UIViewController {
             self.user = self.pool?.currentUser()
         }
         self.refresh()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: Selector("hideKeyboard"))
+        tapGesture.cancelsTouchesInView = true
+        view.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(SignInViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SignInViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func hideKeyboard() {
+        username.endEditing(true)
+        password.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= 44//keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += 44//keyboardSize.height
+            }
+        }
     }
     
     func refresh() {

ac1a029189711445caf7c6c6d14b487807ef2c15
Gurinder Singh <gsingh@Gurinders-MacBook-Pro.local>
2017-12-15 15:44:21 -0500

    -changing the menu to match design spec

--- ParabitTechnicianApp/BeaconFinder/New Group/BeaconFinderViewController.swift
+++ ParabitTechnicianApp/BeaconFinder/New Group/BeaconFinderViewController.swift
@@ -72,7 +72,7 @@ class BeaconFinderViewController: UIViewController {
         super.viewDidLoad()
         
         //temp hack to get user attributes
-        shouldShowSignIn()
+        //shouldShowSignIn()
         
         let pool = AWSCognitoIdentityUserPool(forKey: AWSCognitoUserPoolsSignInProviderKey)
         user = pool.currentUser()
@@ -104,12 +104,12 @@ class BeaconFinderViewController: UIViewController {
     func showMenu() {
         if isMenuShown {
             UIView.animate(withDuration: 0.5) {
-                self.menuView.frame = CGRect(x: -180, y: self.menuView.frame.origin.y, width: self.menuView.frame.width, height: self.menuView.frame.height)
+                self.menuView.frame = CGRect(x: -300, y: self.menuView.frame.origin.y, width: 300, height: self.menuView.frame.height)
                     self.isMenuShown = false
             }
         }else{
             UIView.animate(withDuration: 0.5) {
-                self.menuView.frame = CGRect(x: 0, y: self.menuView.frame.origin.y, width: self.menuView.frame.width, height: self.menuView.frame.height)
+                self.menuView.frame = CGRect(x: 0, y: self.menuView.frame.origin.y, width: 300, height: self.menuView.frame.height)
                     self.isMenuShown = true
                 }
         }
